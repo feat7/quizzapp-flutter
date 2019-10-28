b6e0be0695caf273c868d79939cd98f2c5841bd9
Viren Khatri <virenk2906@gmail.com>
2019-10-28 17:13:40 +0530

    cleaned working code, added correct checker

--- lib/model/picture.dart
+++ lib/model/picture.dart
@@ -1,25 +1,38 @@
 import 'package:flutter/material.dart';
 
-class Picture {
-  String imageURL;
-  Image image;
-  List<String> keywords;
-
-  Picture({this.imageURL, this.keywords}){
-//    for (String keyword in this.keywords){
-//      Set<Picture> pictureSet = keywordMap[keyword] == null ? new Set<Picture>() : keywordMap[keyword];
-//      pictureSet.add(this);
-//      keywordMap.update(keyword, (value) => pictureSet);
-//    }
+class Picture extends StatelessWidget {
+  final String answer;
+  final List<String> imageURLs;
+  final List<String> letters;
+  
+  Picture(this.answer, this.letters ,this.imageURLs);
+  
+  @override
+  Widget build(BuildContext context) {
+    return GridView.count(
+      physics: const NeverScrollableScrollPhysics(),
+      crossAxisSpacing: 10,
+      mainAxisSpacing: 10,
+      crossAxisCount: 2,
+      shrinkWrap: true,
+      children: imageURLs.map(
+          (element) =>
+          Container(
+            margin: EdgeInsets.all(2.0),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.all(Radius.circular(5.0)),
+              color: Colors.grey[700]
+            ),
+            child: Padding(
+              padding: const EdgeInsets.all(4.0),
+              child: FittedBox(
+                fit: BoxFit.fill,
+                child: Image.network(
+                  element)
+              ),
+            )
+          ),
+      ).toList(),
+    );
   }
 }
-
-Map<String, Set<Picture>> keywordMap;
-
-List<Picture> allPictures = [
-  Picture(keywords: ["ice","cold","blue","snow","frozen","sky"], imageURL: "https://i.ibb.co/g73cdMv/adventure-blue-calm-waters-300857.jpg"),
-  Picture(keywords: ["ice","cold","frozen","lemon","fruit"], imageURL: "https://i.ibb.co/qmV8hjT/lemons-ice-water-summer-90763.jpg"),
-  Picture(keywords: ["allergy","cold","tissue"], imageURL: "https://i.ibb.co/tLnfkD8/allergies-allergy-cold-41284.jpg"),
-  Picture(keywords: ["tissue","gift","present","christmas"], imageURL: "https://i.ibb.co/GCVkptN/bags-black-friday-christmas-749353.jpg"),
-  Picture(keywords: ["cold","tree","snow"], imageURL: "https://i.ibb.co/gDS6rz5/bird-s-eye-view-bright-close-up-1438761.jpg"),
-];
